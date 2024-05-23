12727766053b0e0d163234eefc165b8031a2d903
JonasFortes12 <jonascforte@hotmail.com>
2024-05-23 10:16:12 -0300

    chore: initialize Java Spring project

---
+++ src/main/java/com/meicash/meicash/MeicashApplication.java
@@ -0,0 +1,13 @@
+package com.meicash.meicash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeicashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeicashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meicash/meicash/MeicashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meicash.meicash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeicashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
